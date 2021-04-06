1de857956d86dab06118cfe2b46d4cd5ea9cd133
Midran82 <zaiko82@gmail.com>
2021-04-06 14:11:15 +0200

    New changes

---
+++ js/functionTemplate.js
@@ -0,0 +1,11 @@
+/*
+Template to use when making new content links
+Remember to put it as a <script></script> under the links
+Remember to have all content on the same line as innerHtml
+Cannot cut the lines
+*/
+
+
+function endre() {
+    document.getElementById("page").innerHTML ="<h2 style='text-decoration:underline'>Link heading</h2><h4 style='text-decoration:underline'>Heading</h4><p>The content. add more paragraphs if needed or headings. </p>"
+}
